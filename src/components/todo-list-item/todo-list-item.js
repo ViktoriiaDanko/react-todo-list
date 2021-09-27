9428c36f8c87ea6a99f78b8e6eb8b0a402e68135
ViktoriiaDanko <danko.vik146@gmail.com>
2021-09-27 17:55:16 +0300

    adding classes

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ( { label, important = false } ) => {
+export default class TodoListItem extends Component {
+    render() {
+        const { label, important = false } = this.props;
 
-    const style = {
-        color: important ? 'tomato' : 'black', 
-        fontWeight: important ? 'bold' : 'normal'
-    };
-
-    return (
-        <span className="todo-list-item">
-            <span 
-                className="todo-list-item-label" 
-                style={style}> { label } 
+        const style = {
+            color: important ? 'tomato' : 'black', 
+            fontWeight: important ? 'bold' : 'normal'
+        };
+    
+        return (
+            <span className="todo-list-item">
+                <span 
+                    className="todo-list-item-label" 
+                    style={style}> { label } 
+                </span>
+    
+            <button type="button"
+                className="btn btn-outline-success btn-sm float-right">
+                <i className="bi bi-exclamation-lg" />
+            </button>
+    
+            <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+                <i className="bi bi-x-circle-fill" />
+            </button>
             </span>
-
-        <button type="button"
-            className="btn btn-outline-success btn-sm float-right">
-            <i className="bi bi-exclamation-lg" />
-        </button>
-
-        <button type="button"
-            className="btn btn-outline-danger btn-sm float-right">
-            <i className="bi bi-x-circle-fill" />
-        </button>
-        </span>
-    );
+        );
+    };
 };
-
-export default TodoListItem;
